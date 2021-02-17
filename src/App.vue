3ef327eadfe6fc26a6bee87ef8ea939c251eeed5
jensen-markus-bjerkhede <59868512+jensen-markus-bjerkhede@users.noreply.github.com>
2021-02-17 21:04:30 +0100

    init commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <main>
+        <h1> Vue app that can run in a Docker container </h1>
+        <p>It's the Dockerfile that does it!</p>
+    </main>
+</template>
+
+<script>
+
+export default {
+    name: 'App',
+    components: { }
+}
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
