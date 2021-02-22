620578cd6f5103a93ff6b897c3dee7a064f511ce
Markus Bjerkhede <markus.bjerkhede@gmail.com>
2021-02-22 13:31:18 +0100

    add test files

---
+++ tests/Meetups.spec.js
@@ -0,0 +1,7 @@
+import { shallowMount } from '@vue/test-utils'
+import Meetups from "../../src/components/Meetups";
+
+describe('Meetups.vue', () => {
+  it('Should display the upcomming meetups when you enter the site', () => {
+
+  }
